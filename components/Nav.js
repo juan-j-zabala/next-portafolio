9386c9ea0083559805625162b9f7d930d9886248
juan jose zabala <juanzabala317@hotmail.com>
2023-07-27 16:00:26 -0500

    tercer commit

--- components/Nav.js
+++ components/Nav.js
@@ -37,10 +37,19 @@ const Nav = () => {
      bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full  '>
       {navData.map((link, index) =>{
         return(
-          <Link href={link.path} key={index}>
-            <div>  
-              {link.icon}
+          <Link className={`${link.path === pathname ? "text-accent" : "" } relative flex items-center group hover:text-accent transition-all duration-300 `} href={link.path} key={index}>
+            {/* tooltip */}
+            <div className='absolute pr-14 right-0 hidden xl:group-hover:flex '>
+              <div className='bg-white relative flex text-primary items-center p-[6px] rounded-[3px] '> 
+                <div className='text-[12px] leading-none font-semibold capitalize '>
+                  {link.name}
+                </div>
+                {/* triangle */}
+                <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2 '/>
+              </div>
             </div>
+            {/* icon */}
+            <div>{link.icon}</div>
           </Link>
         )  
       })}
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,10 @@
-import { FaCss3, FaHtml5, FaJs, FaReact, FaWordpress } from 'react-icons/fa'
-import { SiFramer, SiNextdotjs } from 'react-icons/si'
+import { useState } from 'react';
+import { FaCss3, FaFigma, FaHtml5, FaJs, FaReact, FaWordpress } from 'react-icons/fa'
+import { SiAdobephotoshop, SiAdobexd, SiFramer, SiNextdotjs } from 'react-icons/si'
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
 
 //  data
 export const aboutData = [
@@ -74,7 +79,76 @@ export const aboutData = [
 ];
 
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0)
+
+  return (
+    <div className='h-full bg-primary/30 py-32 text-center xl:text-left '>
+      <Circles/>
+      {/* avatar img */}
+      <motion.div
+        variants={fadeIn("right", 0.2 )}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className='hidden xl:flex absolute bottom-0 -left-[370px]'
+        >
+        <Avatar/>
+      </motion.div>
+      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6 '>
+        {/* text */}
+        <div className='flex-1 flex flex-col justify-center'>
+          <h2 className='text-[35px] leading-tight md:text-[54px] md:leading-[1.3] mb-4 font-semibold'>
+            Captivanting <span className='text-accent '>stories</span> birth magnificent design.
+          </h2>
+          <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0 ' >
+            10 years ago, I began freelancing as a developer. Since then, I've done 
+            remote work for agencies, consulted for startups, and collaborated on 
+            digital products for business and consumer use.
+          </p>
+          {/* counters */}
+          <div>counters</div>
+        </div>
+        {/* info */}
+        <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]  '>
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 '>
+            {aboutData.map((item, itemindex) => {
+              return  (
+                <div 
+                key={itemindex} 
+                className={`${index === itemindex ? "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300  " : "" } 
+                cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] 
+                after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                onClick={() => setIndex(itemindex)}
+                >
+                  {item.title}
+                </div>
+              )
+            })}
+          </div>
+          <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start '>
+            {aboutData[index].info.map((item, itemindex) => {
+              return (
+                <div key={itemindex} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60 ' > 
+                  {/* title */}
+                  <div className='font-light mb-2 md:mb-0 '>{item.title}</div>
+                  <div className='hidden md:flex '>-</div>
+                  <div>{item.stage}</div>
+                  <div className='flex gap-x-4 '>
+                    {/* icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return (
+                        <div className='text-2xl text-white'>{icon}</div>
+                      )
+                    })}
+                  </div>
+                </div>  
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  ) 
 };
 
 export default About;
